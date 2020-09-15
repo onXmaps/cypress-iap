2f8d424f69cc6df0dcc22b725cde46669bc95369
Colt Dorsey <colt.dorsey@onxmaps.com>
2020-09-15 16:39:57 -0500

    wip cypress-iap

--- src/visit.js
+++ src/visit.js
@@ -1,3 +1,4 @@
+import { getIAPToken } from "./utils";
 Cypress.Commands.overwrite('visit', (originalFn, subject, ...args) => {
     if (Cypress.config().baseUrl.indexOf('localhost') != -1) {
         return originalFn(subject, ...args);
@@ -5,7 +6,7 @@ Cypress.Commands.overwrite('visit', (originalFn, subject, ...args) => {
         cy.exec('curl -I ' + Cypress.config().baseUrl + " | awk '/^location/ {split($NF, a, /[=&]/); print a[2]}'").then((client_id) => {
             return client_id.stdout
         }).then(client_id => {
-            cy.task('getToken', { url: Cypress.config().baseUrl, cid: client_id }).then((token) => {
+            cy.task('getIAPToken', { url: Cypress.config().baseUrl, cid: client_id }).then((token) => {
                 console.log(token)
                 cy.setCookie('GCP_IAAP_AUTH_TOKEN', token)
                     .then( () => {
