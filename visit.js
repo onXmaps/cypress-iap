a411577b0fb41ebd4ac0591c996f81dc84b277bd
Colt Dorsey <colt.dorsey@onxmaps.com>
2020-09-16 15:45:25 -0500

    url adjustment

--- visit.js
+++ visit.js
@@ -10,7 +10,8 @@ Cypress.Commands.overwrite('visit', (originalFn, subject, ...args) => {
         }).then(client_id => {
             const auth = new GoogleAuth();
             auth.getIdTokenClient(client_id).then((client) => {
-                client.request(Cypress.config().baseUrl).then((res) => {
+                const url = Cypress.config().baseUrl
+                client.request({ url }).then((res) => {
                     res.config.headers.Authorization.split(" ")[1].then((token) => {
                         console.log(token)
                         cy.setCookie('GCP_IAAP_AUTH_TOKEN', token)
