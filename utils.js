7a49da492ff9632ef7e3e4b5e692de3d223a7b3b
Colt Dorsey <colt.dorsey@onxmaps.com>
2020-09-17 16:16:10 -0500

    moving require into function

--- utils.js
+++ utils.js
@@ -1,15 +1,14 @@
-const { GoogleAuth } = require('google-auth-library');
 
 module.exports = async function getIAPToken({ url, cid }) {
-    debugger
+    const { GoogleAuth } = require('google-auth-library');
     const auth = new GoogleAuth();
+    debugger
     const client = await auth.getIdTokenClient(cid);
     const res = await client.request({ url });
-    //return res.config.headers.Authorization.split(" ")[1]
-    return new Promise((resolve, reject) => {
-        resolve(res.config.headers.Authorization.split(" ")[1])
-    }) 
-    
+    return res.config.headers.Authorization.split(" ")[1]
+    // return new Promise((resolve, reject) => {
+    //     resolve(res.config.headers.Authorization.split(" ")[1])
+    // })    
 }
 // export async function getIAPToken({ url, cid }) {
 //     debugger
