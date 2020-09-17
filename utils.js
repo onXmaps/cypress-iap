e15cb8a000acada7bcf006c6bc977509393db45c
Colt Dorsey <colt.dorsey@onxmaps.com>
2020-09-17 11:09:19 -0500

    adding export and async arrow function

--- utils.js
+++ utils.js
@@ -1,10 +1,8 @@
+const { GoogleAuth } = require('google-auth-library');
 
-async function getIAPToken({ url: any, cid: any }) {
-    const { GoogleAuth } = require('google-auth-library');
+export const getIAPToken = async({ url, cid }) => {
     const auth = new GoogleAuth();
     const client = await auth.getIdTokenClient(cid);
     const res = await client.request({ url });
     return res.config.headers.Authorization.split(" ")[1]
 }
-
-module.exports.getIAPToken = getIAPToken;
