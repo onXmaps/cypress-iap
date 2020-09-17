634c30fa7d2d6a68ccf0c1b8a830e29ca106d02f
Colt Dorsey <colt.dorsey@onxmaps.com>
2020-09-17 11:37:38 -0500

    changing from cy.task to explicit function call

--- utils.js
+++ utils.js
@@ -1,6 +1,7 @@
 const { GoogleAuth } = require('google-auth-library');
 
 export async function getIAPToken({ url, cid }) {
+    debugger
     const auth = new GoogleAuth();
     const client = await auth.getIdTokenClient(cid);
     const res = await client.request({ url });
--- visit.js
+++ visit.js
@@ -1,3 +1,4 @@
+import {getIAPToken} from './utils'
 /// <reference types="cypress">
 Cypress.Commands.overwrite('visit', (originalFn, subject, ...args) => {
 
@@ -7,12 +8,14 @@ Cypress.Commands.overwrite('visit', (originalFn, subject, ...args) => {
         cy.exec('curl -I ' + Cypress.config().baseUrl + " | awk '/^location/ {split($NF, a, /[=&]/); print a[2]}'").then((client_id) => {
             return client_id.stdout
         }).then(client_id => {
-            cy.task('getIAPToken', { url: Cypress.config().baseUrl, cid: client_id }).then((token) => {
+            getIAPToken({ url: Cypress.config().baseUrl, cid: client_id }).then((token) => {
                 console.log(token)
                 cy.setCookie('GCP_IAAP_AUTH_TOKEN', token)
                     .then(() => {
                         return originalFn(subject, ...args)
                     })
+            }).catch((err) => {
+                console.log(err)
             })
         })
     }
