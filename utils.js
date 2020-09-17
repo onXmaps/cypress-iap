01a51b12f72c7159c17d3146aa56d8d7b72c351f
Colt Dorsey <colt.dorsey@onxmaps.com>
2020-09-17 11:22:57 -0500

    changing export signature

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 const { GoogleAuth } = require('google-auth-library');
 
-export const getIAPToken = async({ url, cid }) => {
+export async function getIAPToken({ url, cid }) {
     const auth = new GoogleAuth();
     const client = await auth.getIdTokenClient(cid);
     const res = await client.request({ url });
