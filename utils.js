54d65ea8c561a7778f76ded2e1af7bf6eeb64b2f
Colt Dorsey <colt.dorsey@onxmaps.com>
2020-09-17 16:05:06 -0500

    Add debugger

--- utils.js
+++ utils.js
@@ -1,11 +1,15 @@
-//const { GoogleAuth } = require('google-auth-library');
+const { GoogleAuth } = require('google-auth-library');
 
 module.exports = async function getIAPToken({ url, cid }) {
-    const { GoogleAuth } = require('google-auth-library');
+    debugger
     const auth = new GoogleAuth();
     const client = await auth.getIdTokenClient(cid);
     const res = await client.request({ url });
-    return res.config.headers.Authorization.split(" ")[1]
+    //return res.config.headers.Authorization.split(" ")[1]
+    return new Promise((resolve, reject) => {
+        resolve(res.config.headers.Authorization.split(" ")[1])
+    }) 
+    
 }
 // export async function getIAPToken({ url, cid }) {
 //     debugger
