b9c652f8026acc8fc64fd845b945da18cb3db608
Colt Dorsey <colt.dorsey@onxmaps.com>
2020-09-17 10:25:37 -0500

    Moving async function back to webmap

--- visit.js
+++ visit.js
@@ -7,13 +7,7 @@ Cypress.Commands.overwrite('visit', (originalFn, subject, ...args) => {
         cy.exec('curl -I ' + Cypress.config().baseUrl + " | awk '/^location/ {split($NF, a, /[=&]/); print a[2]}'").then((client_id) => {
             return client_id.stdout
         }).then(client_id => {
-            const { GoogleAuth } = require('google-auth-library');
-            const auth = new GoogleAuth();
-            return new Promise((resolve, reject) => {
-                getIAPToken(({ url, cid }) => {
-                    resolve(({ url, cid }))
-                })
-            }).then((token) => {
+            cy.task('getIAPToken', { url: Cypress.config().baseUrl, cid: client_id }).then((token) => {
                 console.log(token)
                 cy.setCookie('GCP_IAAP_AUTH_TOKEN', token)
                     .then(() => {
@@ -23,11 +17,3 @@ Cypress.Commands.overwrite('visit', (originalFn, subject, ...args) => {
         })
     }
 })
-
-async function getIAPToken({ url, cid }) {
-    const { GoogleAuth } = require('google-auth-library');
-    const auth = new GoogleAuth();
-    const client = await auth.getIdTokenClient(cid);
-    const res = await client.request({ url });
-    return res.config.headers.Authorization.split(" ")[1]
-}
