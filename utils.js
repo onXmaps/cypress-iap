ddcfa62605e4a57322550721bb1790563dfb24f2
Colt Dorsey <colt.dorsey@onxmaps.com>
2020-09-17 16:37:14 -0500

    Attempting to use on task and returning cypress config

--- utils.js
+++ utils.js
@@ -1,15 +1,23 @@
-
-module.exports = async function getIAPToken({ url, cid }) {
-    const { GoogleAuth } = require('google-auth-library');
-    const auth = new GoogleAuth();
-    debugger
-    const client = await auth.getIdTokenClient(cid);
-    const res = await client.request({ url });
-    return res.config.headers.Authorization.split(" ")[1]
-    // return new Promise((resolve, reject) => {
-    //     resolve(res.config.headers.Authorization.split(" ")[1])
-    // })    
+module.exports = (on, config) => {
+    on('task', {
+        async getIAPToken({ url, cid }) {
+            const { GoogleAuth } = require('google-auth-library');
+            const auth = new GoogleAuth();
+            const client = await auth.getIdTokenClient(cid);
+            const res = await client.request({ url });
+            return res.config.headers.Authorization.split(" ")[1]
+        }
+    })
+    return config
 }
+// module.exports = async function getIAPToken({ url, cid }) {
+//     const { GoogleAuth } = require('google-auth-library');
+//     const auth = new GoogleAuth();
+//     debugger
+//     const client = await auth.getIdTokenClient(cid);
+//     const res = await client.request({ url });
+//     return res.config.headers.Authorization.split(" ")[1]  
+// }
 // export async function getIAPToken({ url, cid }) {
 //     debugger
 //     const auth = new GoogleAuth();
