edd23bb75aa01f2a71ff0196b6620660b1fc5091
Colt Dorsey <colt.dorsey@onxmaps.com>
2020-09-17 16:24:24 -0500

    Adding cy.task back in

--- visit.js
+++ visit.js
@@ -1,22 +1,20 @@
 const getIAPToken = require('./utils')
 /// <reference types="cypress">
 Cypress.Commands.overwrite('visit', (originalFn, subject, ...args) => {
-
     if (Cypress.config().baseUrl.indexOf('localhost') != -1) {
         return originalFn(subject, ...args);
     } else {
         cy.exec('curl -I ' + Cypress.config().baseUrl + " | awk '/^location/ {split($NF, a, /[=&]/); print a[2]}'").then((client_id) => {
             return client_id.stdout
         }).then(client_id => {
-            getIAPToken({ url: Cypress.config().baseUrl, cid: client_id }).then((token) => {
+            cy.task(getIAPToken, { url: Cypress.config().baseUrl, cid: client_id }).then((token) => {
                 console.log(token)
                 cy.setCookie('GCP_IAAP_AUTH_TOKEN', token)
-                    .then(() => {
+                    .then( () => {
                         return originalFn(subject, ...args)
                     })
-            }).catch((err) => {
-                console.log(err)
             })
         })
+        
     }
 })
