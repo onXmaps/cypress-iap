5277e4e756d93a579fcce52d70defe3186e755b0
Colt Dorsey <colt.dorsey@onxmaps.com>
2020-09-17 15:56:07 -0500

    getting rid of cypress on event

--- utils.js
+++ utils.js
@@ -1,15 +1,11 @@
-const { GoogleAuth } = require('google-auth-library');
+//const { GoogleAuth } = require('google-auth-library');
 
-module.exports = (on, config) => {
-    on('task', {
-        async getIAPToken({ url, cid }) {
-            const { GoogleAuth } = require('google-auth-library');
-            const auth = new GoogleAuth();
-            const client = await auth.getIdTokenClient(cid);
-            const res = await client.request({ url });
-            return res.config.headers.Authorization.split(" ")[1]
-        }
-    })
+module.exports = async function getIAPToken({ url, cid }) {
+    const { GoogleAuth } = require('google-auth-library');
+    const auth = new GoogleAuth();
+    const client = await auth.getIdTokenClient(cid);
+    const res = await client.request({ url });
+    return res.config.headers.Authorization.split(" ")[1]
 }
 // export async function getIAPToken({ url, cid }) {
 //     debugger
