52827a307b105338ac62f3d1d25dcdcee04e0368
Colt Dorsey <colt.dorsey@onxmaps.com>
2020-09-17 15:04:24 -0500

    Another attempt

--- utils.js
+++ utils.js
@@ -1,9 +1,20 @@
 const { GoogleAuth } = require('google-auth-library');
 
-export async function getIAPToken({ url, cid }) {
-    debugger
-    const auth = new GoogleAuth();
-    const client = await auth.getIdTokenClient(cid);
-    const res = await client.request({ url });
-    return res.config.headers.Authorization.split(" ")[1]
+module.exports = (on, config) => {
+    on('task', {
+        async getIAPToken({ url, cid }) {
+            const { GoogleAuth } = require('google-auth-library');
+            const auth = new GoogleAuth();
+            const client = await auth.getIdTokenClient(cid);
+            const res = await client.request({ url });
+            return res.config.headers.Authorization.split(" ")[1]
+        }
+    })
 }
+// export async function getIAPToken({ url, cid }) {
+//     debugger
+//     const auth = new GoogleAuth();
+//     const client = await auth.getIdTokenClient(cid);
+//     const res = await client.request({ url });
+//     return res.config.headers.Authorization.split(" ")[1]
+// }
