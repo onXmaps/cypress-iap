d477f70e7929b05233671a01bcf55011bd820faf
Colt Dorsey <colt.dorsey@onxmaps.com>
2022-01-26 16:52:44 -0600

    Terminology update

--- utils.js
+++ utils.js
@@ -1,12 +1,12 @@
 module.exports = (on, config) => {
-    on('task', {
-        async getIAPAuthorization({ url, cid }) {
-            const { GoogleAuth } = require('google-auth-library');
-            const auth = new GoogleAuth();
-            const client = await auth.getIdTokenClient(cid);
-            const res = await client.request({ url });
-            return res.config.headers.Authorization
-        }
-    })
-    return config
-}
+  on("task", {
+    async getIAPToken({ url, cid }) {
+      const { GoogleAuth } = require("google-auth-library");
+      const auth = new GoogleAuth();
+      const client = await auth.getIdTokenClient(cid);
+      const res = await client.request({ url });
+      return res.config.headers.Authorization;
+    },
+  });
+  return config;
+};
--- visit.js
+++ visit.js
@@ -1,18 +1,29 @@
 /// <reference types="cypress">
-Cypress.Commands.overwrite('visit', (originalFn, subject, ...args) => {
-    if (Cypress.config().baseUrl.indexOf('localhost') != -1) {
-        return originalFn(subject, ...args);
-    } else {
-        cy.exec(`curl -I ${Cypress.config().baseUrl} | awk '/^location/ {split($NF, a, /[=&]/); print a[2]}'`).then((client_id) => {
-            return client_id.stdout
-        }).then((client_id) => {
-            cy.task('getIAPAuthorization', { url: Cypress.config().baseUrl, cid: client_id }).then((iapToken) => {
-                cy.intercept(`${Cypress.config().baseUrl}/**`, (req) => {
-                    req.headers['Proxy-Authorization'] = `${iapToken}`
-                }).as('Cypress-IAP').then(() => {
-                    return originalFn(subject, ...args)
-                })
-            })
-        })
-    }
-})
+Cypress.Commands.overwrite("visit", (originalFn, subject, ...args) => {
+  if (Cypress.config().baseUrl.indexOf("localhost") != -1) {
+    return originalFn(subject, ...args);
+  } else {
+    cy.exec(
+      `curl -I ${
+        Cypress.config().baseUrl
+      } | awk '/^location/ {split($NF, a, /[=&]/); print a[2]}'`
+    )
+      .then((client_id) => {
+        return client_id.stdout;
+      })
+      .then((client_id) => {
+        cy.task("getIAPToken", {
+          url: Cypress.config().baseUrl,
+          cid: client_id,
+        }).then((iapToken) => {
+          cy.intercept(`${Cypress.config().baseUrl}/**`, (req) => {
+            req.headers["Proxy-Authorization"] = `${iapToken}`;
+          })
+            .as("Cypress-IAP")
+            .then(() => {
+              return originalFn(subject, ...args);
+            });
+        });
+      });
+  }
+});
