b60e779511c49c9b0efacca01bb765bae8a82d16
Chambers <kirk.chambers@onxmaps.com>
2022-01-26 15:41:02 -0700

    Change Google IAP authorization from setting a cookie to using new Proxy-Authentication header.

--- utils.js
+++ utils.js
@@ -1,11 +1,11 @@
 module.exports = (on, config) => {
     on('task', {
-        async getIAPToken({ url, cid }) {
+        async getIAPAuthorization({ url, cid }) {
             const { GoogleAuth } = require('google-auth-library');
             const auth = new GoogleAuth();
             const client = await auth.getIdTokenClient(cid);
             const res = await client.request({ url });
-            return res.config.headers.Authorization.split(" ")[1]
+            return res.config.headers.Authorization
         }
     })
     return config
